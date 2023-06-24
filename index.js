f86df673eedeefbd85395f5243eb1ad46eb0c934
vikas <vikassingh.vs242@gmail.com>
2023-06-24 22:54:29 +0530

    update

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:3001",
+      "http://localhost:3000",
       "https://socket-chat-app-3v3p.onrender.com",
     ],
     methods: ["GET", "POST"],
